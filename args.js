76e010d28d54892e8df72c660efe5eea7f934eae
joechesney <33613119+joechesney@users.noreply.github.com>
2018-03-01 21:45:04 -0600

    finished the assignment with some the bonuses, i think

--- args.js
+++ args.js
@@ -2,14 +2,11 @@
 
 const [,,...input] = process.argv;
 //couldnt figure out how to put default value into this above statement
-if(input === undefined){
-  input = 0;
+let sum;
+if(input.length === 0){
+  sum = 0;
+}else{
+  sum = input.map((value)=>+value).reduce((acc, curr)=>acc+=curr);
 }
-// console.log('input',input);
-// const nums = input.map((value, index )=>+value);
-// const sum = nums.reduce((acc, curr)=>{
-//   return acc+=curr;
-// })
-const sum = input.map((value)=>+value).reduce((acc, curr)=>acc+=curr);
-// console.log('nums',nums);
+
 console.log('sum: ',sum);
